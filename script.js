6070bbc6c23807e0800792510534f922bc064cc8
huuanh <huuanhnguyen56@gmail.com>
2025-03-31 15:16:25 +0700

    add script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Chờ sự kiện khi người dùng nhấn vào nút
+document.getElementById('actionButton').addEventListener('click', function() {
+    // Hiển thị tên các thành viên nhóm
+    alert('Nguyễn Hữu Anh, Lâm Thanh Đỉnh, Phạm Hữu Luân');
+});
